57d5e792f5887802e5fc281fb814eb6aa72c290f
Alexandr Rebikov <rebikovlk@mail.ru>
2021-11-02 17:21:01 +0700

    there < back

--- src/js/widget.js
+++ src/js/widget.js
@@ -8,16 +8,23 @@ export default class Widget {
     this.dateThere = document.querySelector('.date-input-there');
     this.inputBack = document.querySelector('.input-back');
     this.inputThere = document.querySelector('.input-there');
+    this.datePrev = document.querySelector('.date-prev');
+    this.dateNext = document.querySelector('.date-next');
+    this.inputBackValue = document.querySelector('.input-back').value;
+    this.inputThereValue = document.querySelector('.input-there').value;
+    this.currentInput = null;
+    this.count = 0;
   }
 
   events() {
-    this.dateChoice();
+    this.clickCheckbox();
     this.dateBackInput();
     this.dateThereInput();
-    Widget.otherClick();
+    this.clickNextMonth();
+    this.clickPrevMonth();
   }
 
-  dateChoice() {
+  clickCheckbox() {
     this.checkbox.addEventListener('click', () => {
       const textThereDate = document.querySelector('.date-input-there > p');
       if (document.querySelector('.date-choice').style.display === 'flex') {
@@ -27,6 +34,7 @@ export default class Widget {
         this.dateBack.style = 'display: block';
         this.dateThere.style = 'width: auto';
         textThereDate.textContent = 'Туда:';
+        document.querySelector('.input-back').value = '';
       } else {
         this.dateBack.style = 'display: none';
         this.dateThere.style = 'width: 99%';
@@ -47,46 +55,106 @@ export default class Widget {
     });
   }
 
-  static otherClick() {
-    document.querySelector('body').addEventListener('click', (ev) => {
-      console.log(ev.target);
-    });
+  static otherClick(element) {
+    for (const i of element) {
+      i.addEventListener('click', () => {
+        if (document.querySelector('.date-choice').style.display === 'flex' && !i.classList.contains('calendar-days')) {
+          document.querySelector('.date-choice').style.display = 'none';
+        }
+      });
+    }
   }
 
-  static dateMoment() {
+  static dateMoment(m = 0) {
     const titleMonth = document.querySelector('.date-month');
     const titleYear = document.querySelector('.date-year');
     const tableBody = document.querySelector('.calendar > tbody');
+    const day = moment().format('D');
     const month = moment().format('MMMM');
-    // const day = moment().format('dddd');
     const year = moment().format('YYYY');
-    const rangeDay = moment.locale();
-    // console.log(day, month, year);
-    console.log(rangeDay);
+    const curMonth = moment().add(m, 'month').format('MMMM');
+    const daysInMonth = moment().add(m, 'month').daysInMonth();
+    let firstDayMonth = 6;
+    if (moment().add(m, 'month').startOf('month').day() !== 0) {
+      firstDayMonth = (moment().add(m, 'month').startOf('month').day() - 1);
+    }
 
-    titleMonth.textContent = month;
+    titleMonth.textContent = curMonth;
     titleYear.textContent = year;
 
     if (!document.querySelector('.calendar > tbody > tr')) {
-      for (let i = 0; i < 5; i += 1) {
+      for (let i = 0; i < 6; i += 1) {
         const cloneTr = document.querySelector('.calendar > thead > tr').cloneNode(true);
         tableBody.appendChild(cloneTr);
       }
     }
 
     const monthDays = Array.from(document.querySelectorAll('.calendar > tbody > tr > th'));
-    const startOfMonth = moment().startOf('month');
-    const arrDays = [...Array(startOfMonth.daysInMonth())];
 
-    let count = 1;
-    for (let i = (startOfMonth.weekday() - 1); i < arrDays.length; i += 1) {
-      monthDays[i].textContent = count;
-      count += 1;
+    for (let i = 0; i < monthDays.length; i += 1) {
+      monthDays[i].textContent = '';
+      monthDays[i].classList.add('calendar-days');
+      monthDays[i].classList.remove('current-date');
+    }
+
+    for (let j = 0; j < monthDays.length; j += 1) {
+      if (j === firstDayMonth) {
+        for (let i = 0; i < daysInMonth; i += 1) {
+          monthDays[j + i].textContent = i + 1;
+          if ((i + 1) > +day) {
+            monthDays[j + i].classList.add('bold-date');
+            monthDays[j + i].classList.remove('calendar-days');
+          }
+          if ((i + 1) === +day && curMonth === month) {
+            monthDays[j + i].classList.add('current-date');
+          }
+        }
+      }
+    }
+    Widget.dateChoiceBack(monthDays, curMonth, year);
+    Widget.dateChoiceThere(monthDays, curMonth, year);
+    Widget.otherClick(monthDays);
+  }
+
+  clickPrevMonth() {
+    this.datePrev.addEventListener('click', () => {
+      this.count -= 1;
+      Widget.dateMoment(this.count);
+    });
+  }
+
+  clickNextMonth() {
+    this.dateNext.addEventListener('click', () => {
+      this.count += 1;
+      Widget.dateMoment(this.count);
+    });
+  }
+
+  static dateChoiceBack(element, month, year) {
+    for (const i of element) {
+      console.log(i.classList.contains('calendar-days'));
+      i.addEventListener('click', (ev) => {
+        if (document.querySelector('.input-back') === this.currentInput && !i.classList.contains('calendar-days')) {
+          document.querySelector('.input-back').value = `${ev.target.textContent} ${month} ${year}`;
+        }
+      });
+    }
+  }
+
+  static dateChoiceThere(element, month, year) {
+    for (const i of element) {
+      console.log(i.classList.contains('calendar-days'));
+      i.addEventListener('click', (ev) => {
+        if (document.querySelector('.input-there') === this.currentInput && !i.classList.contains('calendar-days')) {
+          document.querySelector('.input-there').value = `${ev.target.textContent} ${month} ${year}`;
+        }
+      });
     }
   }
 
   static popUpDate(element) {
     element.focus();
+    this.currentInput = element;
     const calendar = document.querySelector('.date-choice');
     if (calendar.style === 'display: flex') {
       calendar.style = 'display: none';
